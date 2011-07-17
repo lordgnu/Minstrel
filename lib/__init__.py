48651ffa5a24ed51ea4f24072ea213ea0dcff54c
Don Bauer <lordgnu@me.com>
2011-07-17 12:22:07 -0500

    Added Libraries (CherryPy, pygithub, simplejson, argparse, configobject)

---
+++ lib/__init__.py
@@ -0,0 +1,19 @@
+# Author: Don Bauer <lordgnu@me.com>
+# URL: http://github.com/lordgnu/Minstrel
+# Created: Jul 16, 2011
+#
+# This file is part of Minstrel
+#
+# Minstrel is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Minstrel is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Minstrel.  If not, see <http://www.gnu.org/licenses/>.
+
